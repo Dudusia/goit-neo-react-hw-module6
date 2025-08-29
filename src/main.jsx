f83bb94482e4e47d68d1d0910304e53418216f4b
Yevdokiia Mazhak <yevdokiia.mazhak@oag.com>
2025-08-29 11:38:34 +0300

    feat: add redux components

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
 import 'modern-normalize';
+import { store } from './redux/store';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
---
+++ src/redux/contactsSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+import contacts from '../data/contacts.json'
+
+export const getContacts = state => state.contacts.items;
+
+const slice = createSlice({
+  name: 'contacts',
+  initialState: {items: contacts},
+  reducers: {
+    addContact: (state, action) => {
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+    },
+    deleteContact: (state, action) => {
+      return {
+        ...state,
+        items: state.items.filter((contact) => contact.id !== action.payload),
+      };
+    },
+  },
+});
+
+export const { addContact, deleteContact } = slice.actions;
+
+export default slice.reducer;
